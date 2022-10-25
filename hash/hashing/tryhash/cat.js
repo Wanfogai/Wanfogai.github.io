b682aebbdf883e8ab220fd19049bb20b5de5a0b8
Wanfogai <81505429+Wanfogai@users.noreply.github.com>
2022-10-25 15:45:08 +0300

    Update cat.js

--- hash/hashing/tryhash/cat.js
+++ hash/hashing/tryhash/cat.js
@@ -1,7 +1,7 @@
 let friend = document.getElementById("cout");
 
 function fuck(){
-    if (hashCode(friend.value) == "47917752") {
+    if (hashCode(friend.value) == "-1098123364") {
         document.location.href = hashCode(document.title)+".html";
     }
 }
